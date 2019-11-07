53d79c59d22033b5d174dcabc6021482ac4f7fff
manuelsanchez <manuel.vma12@gmail.com>
2019-11-06 19:50:37 -0600

    checksyle 100% and test

---
+++ java/src/main/java/mx/iteso/WaterIceCreamStore.java
@@ -0,0 +1,30 @@
+package mx.iteso;
+
+import mx.iteso.icecreams.IceCream;
+import mx.iteso.icecreams.water.GuavaWaterIceCream;
+import mx.iteso.icecreams.water.MangoWaterIceCream;
+import mx.iteso.icecreams.water.PineappleWaterIceCream;
+import mx.iteso.icecreams.water.StrawberryWaterIceCream;
+
+/**
+ * Empty JavaDoc.
+ */
+public class WaterIceCreamStore extends IceCreamStore {
+    /**
+     * @param flavor Sabor del helado.
+     * @return helado.
+     */
+    public final IceCream createIceCream(final String flavor) {
+
+        if (flavor.equals("Strawberry")) {
+            return new StrawberryWaterIceCream();
+        } else if (flavor.equals("Pineapple")) {
+            return new PineappleWaterIceCream();
+        } else if (flavor.equals("Mango")) {
+            return new MangoWaterIceCream();
+        } else if (flavor.equals("Guava")) {
+            return new GuavaWaterIceCream();
+        }
+        return null;
+    }
+}
---
+++ java/src/main/java/mx/iteso/icecreams/yoghurt/StrawberryYoghurtIceCream.java
@@ -0,0 +1,9 @@
+package mx.iteso.icecreams.yoghurt;
+
+import mx.iteso.icecreams.IceCream;
+
+/**
+ * Empty Class.
+ */
+public class StrawberryYoghurtIceCream extends IceCream {
+}
